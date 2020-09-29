91fd8f6c23ab7cabc14381633723db3c18417bbb
Julie Johansen <julie.kvarme@gmail.com>
2020-09-29 17:38:13 +0200

    Lagt til kommentarer i konstruktøren.

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -3,10 +3,15 @@ package no.oslomet.cs.algdat;
 public class Main {
     public static void main(String[] args) {
 
-        String [] str = {};
+        String [] str = {"hei", null, "paa", "deg",null};
+
+        Integer [] ints = {1, 2, null, 3, 4};
 
 
         Liste<String> liste = new DobbeltLenketListe<>(str);
         System.out.println("Antall : "+liste.antall()+". Er tabellen tom? "+liste.tom());
+
+        Liste<Integer> intliste = new DobbeltLenketListe<>(ints);
+        System.out.println("Antall : "+intliste.antall()+". Er tabellen tom? "+intliste.tom());
     }
 }
