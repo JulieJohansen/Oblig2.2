bd32487a2ba2a47880129582c1ddc956225d231b
Julie Johansen <julie.kvarme@gmail.com>
2020-10-01 12:49:56 +0200

    Noen forandringer på Finn node, 2a. fungerer ikke.

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -9,23 +9,23 @@ public class Main {
 
         Integer [] ints2 = {};
 
-        Liste<String> liste = new DobbeltLenketListe<>(str);
+        DobbeltLenketListe<String> liste = new DobbeltLenketListe<>(str);
         System.out.println("Antall : "+liste.antall()+". Er tabellen tom? "+liste.tom());
 
-        Liste<Integer> intliste = new DobbeltLenketListe<>(ints);
+        DobbeltLenketListe<Integer> intliste = new DobbeltLenketListe<>(ints);
         System.out.println("Antall : "+intliste.antall()+". Er tabellen tom? "+intliste.tom());
-        Liste<Integer> intliste2 = new DobbeltLenketListe<>(ints2);
+        DobbeltLenketListe<Integer> intliste2 = new DobbeltLenketListe<>(ints2);
         System.out.println("Antall : "+intliste2.antall()+". Er tabellen tom? "+intliste2.tom());
 
         System.out.println(intliste2.toString());
 
         intliste2.leggInn(9);
         intliste2.leggInn(6);
-        System.out.println("Antall : "+intliste2.antall()+". Er tabellen tom? "+intliste2.tom());
-        System.out.println(intliste2.toString());
-
+        //System.out.println("Antall : "+intliste2.antall()+". Er tabellen tom? "+intliste2.tom());
+        //System.out.println(intliste2.toString());
+        System.out.println(liste.toString());
         System.out.println(intliste.toString());
-        //System.out.println(String.valueOf(intliste.omvendtString()));
+        System.out.println(intliste.omvendtString());
 
         //Integer newint = intliste2.hent(0);
         //System.out.println(newint);
