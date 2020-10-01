d518a2b1d95e6c5b7486e876ed812e5dbe408a02
Julie Johansen <julie.kvarme@gmail.com>
2020-10-01 10:16:23 +0200

    Forsøkt meg på stringbuilder i oppgave 2a, man har ikke fått den til å fungere enda.

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -7,11 +7,16 @@ public class Main {
 
         Integer [] ints = {1, 2, null, 3, 4};
 
+        Integer [] ints2 = {};
 
         Liste<String> liste = new DobbeltLenketListe<>(str);
         System.out.println("Antall : "+liste.antall()+". Er tabellen tom? "+liste.tom());
 
         Liste<Integer> intliste = new DobbeltLenketListe<>(ints);
         System.out.println("Antall : "+intliste.antall()+". Er tabellen tom? "+intliste.tom());
+        Liste<Integer> intliste2 = new DobbeltLenketListe<>(ints2);
+        System.out.println("Antall : "+intliste2.antall()+". Er tabellen tom? "+intliste2.tom());
+
+        System.out.println(intliste2.toString());
     }
 }
